6a373e350179293f34a9cfa077261ee60ee6bf76
Jon Tewksbury <jontewks@gmail.com>
2020-12-15 09:19:15 -0600

    Day 13 part 2

--- day-13/part 2.js
+++ day-13/part 2.js
@@ -4,27 +4,59 @@ const inputPath = new URL('./input.txt', import.meta.url)
 const input = await readInput(inputPath)
 const inputArray = input.split('\n')
 
-const busIDs = inputArray[1].split(',')
-let sequenceFound = false
-let iterator = 1
-const maxBusWait = Math.max(...busIDs.filter((id) => id !== 'x'))
-const maxBusWaitIndex = busIDs.indexOf(String(maxBusWait))
+const inputs = inputArray[1]
+	.split(',')
+	.map((id, i) => ({ id, i }))
+	.filter((eq) => eq.id !== 'x')
+	.map((eq) => {
+		const n = parseInt(eq.id.trim())
+		return {
+			n: BigInt(n),
+			i: eq.i,
+			a: BigInt(n - eq.i),
+		}
+	})
 
-while (!sequenceFound) {
-  const startingNumber = Number(maxBusWait) * iterator
+const solveMMI = (a, n) => {
+	let t = 0n
+	let newT = 1n
+	let r = n
+	let newR = a
 
-  const sequenceStartFound = busIDs.every((id, i) => {
-    if (id === 'x') {
-      return true
-    }
-    const distanceFromMaxIndex = i - maxBusWaitIndex
-    return (startingNumber + distanceFromMaxIndex) % Number(id) === 0
-  })
+	while (newR !== 0n) {
+		const quotient = r / newR
 
-  if (sequenceStartFound) {
-    sequenceFound = true
-    console.log(startingNumber - maxBusWaitIndex)
-  } else {
-    iterator++
-  }
+		const tmpT = t
+		t = newT
+		newT = tmpT - quotient * newT
+
+		const tmpR = r
+		r = newR
+		newR = tmpR - quotient * newR
+	}
+
+	if (r > 1n) {
+		throw new Error('not invertible')
+	}
+
+	if (t < 0n) {
+		t += n
+	}
+
+	return t
 }
+
+const solveCRT = (system) => {
+	const prod = system.reduce((p, con) => p * con.n, 1n)
+	return (
+		system.reduce((sm, con) => {
+			const p = prod / con.n
+			const mmi = solveMMI(p, con.n)
+			sm = sm + con.a * mmi * p
+
+			return sm
+		}, 0n) % prod
+	)
+}
+
+console.log(solveCRT(inputs))
